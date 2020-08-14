e06ee106fce5e3633bd57fd07a2ba33708ea3f65
Adam Helton <helton19j93@gmail.com>
2020-08-14 13:32:39 -0400

    Add project files.

---
+++ Adam_Helton_Unit2_IT481/DB.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adam_Helton_Unit2_IT481
+{
+    class DB
+    {
+        string connectionString;
+        SqlConnection cnn;
+        public DB()
+        {
+            connectionString = ("Server = DESKTOP-DOUST62\\SQLEXPRESS;" +
+                "Trusted_Connection=true;" +
+                "Database=northwind;" +
+                "User Instance=false;");
+                
+        }
+
+        public DB(string conn)
+        {
+            connectionString = conn;
+        }
+
+        public string getCustomerCount()
+        {
+            Int32 count = 0;
+
+            cnn = new SqlConnection(connectionString);
+            cnn.Open();
+            string countQuery = "select count(*) from customers;";
+            SqlCommand cmd = new SqlCommand(countQuery, cnn);
+
+            try
+            {
+                count = Convert.ToInt32(cmd.ExecuteScalar());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return count.ToString();
+        }
+
+        public string getCompanyNames()
+        {
+            string names = "None";
+            SqlDataReader dataReader;
+
+            cnn = new SqlConnection(connectionString);
+            cnn.Open();
+            string countQuery = "select companyname from customers;";
+            SqlCommand cmd = new SqlCommand(countQuery, cnn);
+            dataReader = cmd.ExecuteReader();
+            while (dataReader.Read())
+            {
+                try
+                {
+                    names = names + dataReader.GetValue(0) + "\n";
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
+            return names;
+        }
+    }
+}
---
+++ Adam_Helton_Unit2_IT481/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Adam_Helton_Unit2_IT481
+{
+    public partial class Form1 : Form
+    {
+        DB database;
+        public Form1()
+        {
+            InitializeComponent();
+            button1.Click += new EventHandler(button1_Click);
+            button2.Click += new EventHandler(button2_Click);
+            button3.Click += new EventHandler(button3_Click);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            database = new DB("Server = DESKTOP-DOUST62\\SQLEXPRESS; " +
+                "Trusted_Connection=true;" +
+                "Database=northwind;" +
+                "User Instance=false;");
+                
+
+            MessageBox.Show("Connection information sent");
+
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string count = database.getCustomerCount();
+            MessageBox.Show(count, "Customer count");
+
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string names = database.getCompanyNames();
+            MessageBox.Show(names, "Company Names");
+
+        }
+
+    }
+}
